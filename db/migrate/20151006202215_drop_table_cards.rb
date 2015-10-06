26a0064945f1262389626eb7ee1d30ffdcc71fd5
George <spamgoga@gmail.com>
2015-10-07 00:09:03 +0300

    ->

--- db/migrate/20151006202215_drop_table_cards.rb
+++ db/migrate/20151006202215_drop_table_cards.rb
@@ -1,6 +1,8 @@
 class DropTableCards < ActiveRecord::Migration
   def change
-    if ActiveRecord::Base.connection.table_exists? 'cards'
+    if ActiveRecord::Base.connection.table_exists? :cards
       drop_table :cards
+    end
+    
   end
 end
