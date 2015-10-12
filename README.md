36a60a8fdebc85bd9773890041e62ea909984fc0
pandorica-opens <anna_8tav@mail.ru>
2015-10-12 12:23:20 +0300

    finished 1 chapter

--- README.md
+++
@@ -1,5 +0,0 @@
-# sh2
-changelog: https://goo.gl/2uauzt<br>
-production: http://sh2.herokuapp.com/
-
-serverhouse2
--- config/boot.rb
+++
@@ -1,3 +0,0 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'bundler/setup' # Sh2et up gems listed in the Gemfile.
